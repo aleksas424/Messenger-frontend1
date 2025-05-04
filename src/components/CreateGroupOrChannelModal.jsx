234afe20f6042092da3dea03cd11764ecd827cda
Vakaris Toleikis <jonas68824@gmail.com>
2025-05-04 20:48:58 +0300

    http

--- src/components/CreateGroupOrChannelModal.jsx
+++ src/components/CreateGroupOrChannelModal.jsx
@@ -9,7 +9,7 @@ export default function CreateGroupOrChannelModal({ type, onClose, onCreated }) 
   const [admins, setAdmins] = useState([]);
 
   useEffect(() => {
-    axios.get('https://5.199.169.195:5000/api/users')
+    axios.get('http://5.199.169.195:5000/api/users')
       .then(res => setUsers(res.data));
   }, []);
 
@@ -23,7 +23,7 @@ export default function CreateGroupOrChannelModal({ type, onClose, onCreated }) 
 
   const handleCreate = async () => {
     await axios.post(
-      'https://5.199.169.195:5000/api/group',
+      'http://5.199.169.195:5000/api/group',
       { name, type, description, members: selected, admins }
     );
     onCreated();
